d179f7cb7c421d28eea23fdceee40f598999649e
Sue Ceklosky <cekloskys@chc.edu>
2023-01-11 13:19:30 -0500

    Training Day 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const { Sider, Content, Footer } = Layout;
 function App() {
   return (
     <Layout>
-      <Sider style={{height: '100vh', backgroundColor: 'white'}}>
+      <Sider style={{backgroundColor: 'white'}}>
         <Image 
           src='https://logos-world.net/wp-content/uploads/2020/11/Uber-Eats-Symbol.jpg' 
           preview={false} 
@@ -21,7 +21,7 @@ function App() {
         <Footer style={{textAlign: 'center'}}>
           Uber Eats Dashboard @2022
         </Footer>
-      </Layout>
+      </Layout> 
     </Layout>
   );
 }
--- src/modules/DetailedOrder/index.js
+++ src/modules/DetailedOrder/index.js
@@ -1,8 +1,85 @@
+import { Card, Descriptions, Divider, List, Button } from 'antd';
+import { useParams } from 'react-router-dom';
+import dishes from '../../data/dashboard/dishes.json';
+
 const DetailedOrder = () => {
 
+    const { id } = useParams();
+
+    const total = dishes.reduce((sum, dish) => {
+        return sum + (dish.quantity * dish.price)
+    }, 0);
+
     return (
-        <div>Detailed Order</div>
+        <Card title={`Order Number ${id}`} style={{ margin: 20 }}>
+            <Descriptions bordered column={{ lg: 1, md: 1, sm: 1 }}>
+                <Descriptions.Item label='Order Status'>APPROVED</Descriptions.Item>
+                <Descriptions.Item label='Customer'>Susan Ceklosky</Descriptions.Item>
+                <Descriptions.Item label='Customer Address'>301 Woods Edge Drive</Descriptions.Item>
+            </Descriptions>
+            <Divider />
+            <List 
+                dataSource={dishes}
+                renderItem={(dishItem) => (
+                    <List.Item>
+                        <div style={{fontWeight: 'bold'}}>{dishItem.name} x{dishItem.quantity}</div>
+                        <div>${dishItem.price}</div>
+                    </List.Item>
+                )}
+            />
+            <Divider />
+            <div style={styles.totalContainer}>
+                <h2>Total:</h2>
+                <h2 style={styles.totalPrice}>${total}</h2>
+            </div>
+            <Divider />
+            <div style={styles.buttonsContainer}>
+                <Button
+                    block
+                    danger
+                    type='primary'
+                    size='large'
+                    style={styles.button}
+                >
+                    Decline Order
+                </Button>
+                <Button
+                    block
+                    type='primary'
+                    size='large'
+                    style={styles.button}
+                >
+                    Accept Order
+                </Button>
+                <Button
+                    block
+                    type='default'
+                    size='large'
+                    style={styles.button}
+                >
+                    Food Is Done
+                </Button>
+            </div>
+        </Card>
     );
+};
+
+const styles = {
+    totalContainer: {
+        flexDirection: 'row',
+        display: 'flex',
+    },
+    totalPrice: {
+        marginLeft: 'auto',
+    },
+    buttonsContainer: {
+        display: 'flex',
+        paddingBottom: 30,
+    },
+    button: {
+        marginRight: 5,
+        marginLeft: 5,
+    },
 };
 
 export default DetailedOrder;
